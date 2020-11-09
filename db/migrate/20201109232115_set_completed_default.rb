6b852ba55c2602ff1ab8b2f2992280cecf0891af
Andrew Bilski <andrewyangbilski@gmail.com>
2020-11-09 18:29:01 -0500

    Ex 2 - Set completed default value to false

---
+++ db/migrate/20201109232115_set_completed_default.rb
@@ -0,0 +1,5 @@
+class SetCompletedDefault < ActiveRecord::Migration[6.0]
+  def change
+    change_column_default :tasks, :completed, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_231600) do
+ActiveRecord::Schema.define(version: 2020_11_09_232115) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
